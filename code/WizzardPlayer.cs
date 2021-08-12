06bda062eec3f8c7b80979473e8db58dba0adaa8
Tomast1337 <vycasnicolas@gmail.com>
2021-08-12 14:16:56 -0300

    Alterações em spawnRagdoll

--- code/WizzardPlayer.cs
+++ code/WizzardPlayer.cs
@@ -29,21 +29,63 @@ namespace Tomast1337
 			base.Respawn();
 		}
 		
-		private void spawnRagdoll() {
-			var ragdoll = new ModelEntity();
-			ragdoll.SetModel( GetModelName() );
-			ragdoll.Rotation = Rotation;
+		private void spawnRagdoll(Vector3 velocity) {
+			ModelEntity ragdoll = new ModelEntity();
 			ragdoll.Position = Position;
-			ragdoll.SetupPhysicsFromModel( PhysicsMotionType.Dynamic, false );
-			ragdoll.PhysicsGroup.Velocity = Vector3.Up * 500;
-			ragdoll.DeleteAsync(30);
+			ragdoll.Rotation = Rotation;
+			ragdoll.Scale = Scale;
+			ragdoll.MoveType = MoveType.Physics;
+			ragdoll.UsePhysicsCollision = true;
+			ragdoll.EnableAllCollisions = true;
+			ragdoll.CollisionGroup = CollisionGroup.Debris;
+			ragdoll.SetModel( GetModelName() );
+			ragdoll.CopyBonesFrom( this );
+			ragdoll.CopyBodyGroups( this );
+			ragdoll.CopyMaterialGroup( this );
+			ragdoll.TakeDecalsFrom( this );
+			ragdoll.EnableHitboxes = true;
+			ragdoll.EnableAllCollisions = true;
+			ragdoll.SurroundingBoundsMode = SurroundingBoundsType.Physics;
+			ragdoll.RenderColorAndAlpha = RenderColorAndAlpha;
+			ragdoll.PhysicsGroup.Velocity = velocity;
+
+			ragdoll.SetInteractsAs( CollisionLayer.Debris );
+			ragdoll.SetInteractsWith( CollisionLayer.WORLD_GEOMETRY );
+			ragdoll.SetInteractsExclude( CollisionLayer.Player | CollisionLayer.Debris );
+
+			foreach ( Entity child in Children )
+			{
+				if ( child is ModelEntity ent )
+				{
+					string model = ent.GetModelName();
+
+					if ( model != null && !model.Contains( "clothes" ) )
+					{
+						continue;
+					}
+
+					ModelEntity clothing = new ModelEntity();
+					clothing.SetModel( model );
+					clothing.SetParent( ragdoll, true );
+					clothing.RenderColorAndAlpha = ent.RenderColorAndAlpha;
+				}
+			}
+
+			Corpse = ragdoll;
+
+			ragdoll.DeleteAsync( 30 );
+
 		}
 
 		public override void OnKilled()
 		{
 			base.OnKilled();
+			spawnRagdoll( Velocity );
+			Camera = new SpectateRagdollCamera();
+			Controller = null;
+			EnableAllCollisions = false;
 			EnableDrawing = false;
-			spawnRagdoll();
+			
 		}
 
 	}
