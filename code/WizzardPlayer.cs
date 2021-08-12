e2503fa0b307daf9be4bbeb14d80041155ee1d81
Tomast1337 <vycasnicolas@gmail.com>
2021-08-12 15:03:22 -0300

    Alterações em WizzardPlayer -Adicionaod roupas -Melhorado spawnRagdoll a partir do exemplo BecomeRagdollOnClient do modo de jogo sandbox

--- code/WizzardPlayer.cs
+++ code/WizzardPlayer.cs
@@ -7,11 +7,21 @@ namespace Tomast1337
 	{
 		public float Mana { get; set; } = 100;
 
+		private DamageInfo lastDamage;
+		
 		public WizzardPlayer()
 		{
 			
 		}
+		public void Dress(String modelPath) {
+			ModelEntity clothe = new ModelEntity();
 
+			clothe.SetModel( modelPath );
+			clothe.SetParent( this, true );
+
+			clothe.EnableShadowInFirstPerson = true;
+			clothe.EnableHideInFirstPerson = true;
+		}
 		public override void Respawn()
 		{
 			Mana = 100;
@@ -25,11 +35,24 @@ namespace Tomast1337
 			EnableDrawing = true;
 			EnableHideInFirstPerson = true;
 			EnableShadowInFirstPerson = true;
+			//head
+			Dress( "models/citizen_clothes/hat/hat_leathercap.vmdl" );
+
+			//Chest
+			Dress( "models/citizen_clothes/jacket/jacket_heavy.vmdl" );
+
+			//Legs
+			Dress( "models/citizen_clothes/trousers/trousers.jeans.vmdl" );
+
+			//Feet
+			Dress( "models/citizen_clothes/shoes/shoes.workboots.vmdl" );
+			
+			
 
 			base.Respawn();
 		}
 		
-		private void spawnRagdoll(Vector3 velocity) {
+		private void spawnRagdoll( Vector3 velocity, DamageFlags damageFlags, Vector3 forcePos, Vector3 force, int bone ) {
 			ModelEntity ragdoll = new ModelEntity();
 			ragdoll.Position = Position;
 			ragdoll.Rotation = Rotation;
@@ -71,16 +94,60 @@ namespace Tomast1337
 				}
 			}
 
+			if ( damageFlags.HasFlag( DamageFlags.Bullet ) ||
+			 damageFlags.HasFlag( DamageFlags.PhysicsImpact ) )
+			{
+				PhysicsBody body = bone > 0 ? ragdoll.GetBonePhysicsBody( bone ) : null;
+
+				if ( body != null )
+				{
+					body.ApplyImpulseAt( forcePos, force * body.Mass );
+				}
+				else
+				{
+					ragdoll.PhysicsGroup.ApplyImpulse( force );
+				}
+			}
+
+			if ( damageFlags.HasFlag( DamageFlags.Blast ) )
+			{
+				if ( ragdoll.PhysicsGroup != null )
+				{
+					ragdoll.PhysicsGroup.AddVelocity( (Position - (forcePos + Vector3.Down * 100.0f)).Normal * (force.Length * 0.2f) );
+					var angularDir = (Rotation.FromYaw( 90 ) * force.WithZ( 0 ).Normal).Normal;
+					ragdoll.PhysicsGroup.AddAngularVelocity( angularDir * (force.Length * 0.02f) );
+				}
+			}
+
 			Corpse = ragdoll;
 
 			ragdoll.DeleteAsync( 30 );
 
 		}
 
+		public override void TakeDamage( DamageInfo info )
+		{
+			if ( GetHitboxGroup( info.HitboxIndex ) == 1 )
+			{
+				info.Damage *= 10.0f;
+			}
+
+			lastDamage = info;
+
+			TookDamage( lastDamage.Flags, lastDamage.Position, lastDamage.Force );
+
+			base.TakeDamage( info );
+		}
+
+		[ClientRpc]
+		public void TookDamage( DamageFlags damageFlags, Vector3 forcePos, Vector3 force )
+		{
+		}
+
 		public override void OnKilled()
 		{
 			base.OnKilled();
-			spawnRagdoll( Velocity );
+			spawnRagdoll( Velocity, lastDamage.Flags, lastDamage.Position, lastDamage.Force, GetHitboxBone( lastDamage.HitboxIndex ));
 			Camera = new SpectateRagdollCamera();
 			Controller = null;
 			EnableAllCollisions = false;
