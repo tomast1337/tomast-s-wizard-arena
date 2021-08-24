b7c678a644c3ee8bb0f3cf432952bd66c18da734
Tomast1337 <vycasnicolas@gmail.com>
2021-08-24 04:12:48 -0300

    Criado Classe MageStaff ainda intilizada

---
+++ code/MageStaff.cs
@@ -0,0 +1,33 @@
+﻿using Sandbox;
+
+namespace Tomast1337
+{
+	partial class MageStaff : BaseCarriable
+	{
+
+		public override void Spawn()
+		{
+			base.Spawn();
+			CollisionGroup = CollisionGroup.Weapon;
+			SetInteractsAs( CollisionLayer.Debris );
+			Log.Info( "Staff added" );
+		}
+		public override void Simulate( Client player )
+		{
+			Log.Info( "test" );
+			base.Simulate( player );
+
+		}
+
+		public void AttackPrimary()
+		{
+			Log.Info( "Pew" );
+		}
+		public void AttackSecondary()
+		{
+
+			Log.Info( "Pew" );
+		}
+
+	}
+}
