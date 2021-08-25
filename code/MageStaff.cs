6b1c03529c5de0a3b4ffd2d5f94a3dc33fde28d0
Tomast1337 <vycasnicolas@gmail.com>
2021-08-24 23:02:13 -0300

    Adicionado função processAttack Essa função decide o poder selecionado

--- code/MageStaff.cs
+++ code/MageStaff.cs
@@ -14,7 +14,6 @@ namespace Tomast1337
 		}
 		public override void Simulate( Client player )
 		{
-			Log.Info( "test" );
 			base.Simulate( player );
 
 		}
@@ -27,6 +26,133 @@ namespace Tomast1337
 		{
 
 			Log.Info( "Pew" );
+		}
+
+		private void processAttack(bool Fire, bool Earth, bool Lightning, bool Life )
+		{
+			int attackSum = 0;
+			if ( Fire ) attackSum += 1;
+			if ( Earth ) attackSum += 2;
+			if ( Lightning ) attackSum += 4;
+			if ( Life ) attackSum += 8;
+			switch ( attackSum )
+			{
+				case 0:// Base -> wind gust
+					WindGust();
+					break;
+				case 1:// Fire -> flame thrower
+					Flamethrower();
+					break;
+				case 2:// Earth -> rock boulder
+					Rockboulder();
+					break;
+				case 3:// Fire Earth -> meteor
+					Meteor();
+					break;
+				case 4:// Lightning -> Lightning strike
+					Lightningstrike();
+					break;
+				case 5:// Fire Lightning -> Laser shot
+					LaserShot();
+					break;
+				case 6:// Earth Lightning -> Fast rocks shots, Submachine gun like
+					StoneSMG();
+					break;
+				case 7:// Fire Earth Lightning -> muliple rocks shots, Shotgun like
+					StoneShotgun();
+					break;
+				case 8:// Life -> heal
+					Heal();
+					break;
+				case 9:// Fire Life -> flame shield
+					FlameShield();
+					break;
+				case 10:// Earth Life -> Rock Wall
+					RockWall();
+					break;
+				case 11:// Fire Earth Life -> Fire wall
+					FireWall();
+					break;
+				case 12:// Lightning Life -> force field
+					ForceField();
+					break;
+				case 13:// Fire Lightning Life -> Vampirism Shot, Awp like , Hit or die
+					VampirismShot();
+					break;
+				case 14:// Earth Lightning Life -> Tree Spawn
+					Tree();
+					break;
+				case 15:// Fire Earth Lightning Life -> suicide explosion
+					Suicide();
+					break;
+				default:
+					break;
+			}
+		}
+
+		private void WindGust()
+		{
+
+		}
+		private void Flamethrower()
+		{
+
+		}
+		private void Rockboulder()
+		{
+
+		}
+		private void Meteor()
+		{
+
+		}
+		private void Lightningstrike()
+		{
+
+		}
+		private void LaserShot()
+		{
+
+		}
+		private void StoneSMG()
+		{
+
+		}
+		private void StoneShotgun()
+		{
+
+		}
+		private void Heal()
+		{
+
+		}
+		private void FlameShield()
+		{
+
+		}
+		private void RockWall()
+		{
+
+		}
+		private void FireWall()
+		{
+
+		}
+		private void ForceField()
+		{
+
+		}
+		private void VampirismShot()
+		{
+
+		}
+		private void Tree()
+		{
+
+		}
+		private void Suicide()
+		{
+
 		}
 
 	}
