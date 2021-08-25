1b15d46c54529948d42c938e8f093c03fcd1496d
Tomast1337 <vycasnicolas@gmail.com>
2021-08-25 11:13:40 -0300

    Adicionado novos atributos a MageStaff

--- code/weapons/MageStaff.cs
+++ code/weapons/MageStaff.cs
@@ -2,42 +2,64 @@
 
 namespace Tomast1337
 {
-	[Library( "weapon_magestaff", Title = "MageStaff") ]
+	[Library( "weapon_magestaff", Title = "MageStaff", Spawnable = true) ]
 	public partial class MageStaff : BaseWeapon
 	{
+		public bool Fire { get; set; } = false;
+		public bool Earth { get; set; } = false;
+		public bool Lightning { get; set; } = false;
+		public bool Life { get; set; } = false;
+		public float Mana { get; set; } = 100;
+
+		public override string ViewModelPath => "weapons/rust_pistol/v_rust_pistol.vmdl";
+
+		public override void Reload() { }
 
 		public override void Spawn()
 		{
 			base.Spawn();
 			CollisionGroup = CollisionGroup.Weapon;
 			SetInteractsAs( CollisionLayer.Debris );
+			SetModel( "weapons/rust_pistol/rust_pistol.vmdl" );
 			Log.Info( "Staff added" );
 		}
-		public override void Simulate( Client player )
-		{
-			base.Simulate( player );
-			WizzardPlayer playerEnt = (WizzardPlayer)Local.Pawn;
-
-
-		}
-
 		public override void AttackPrimary()
 		{
-			Log.Info( "Pew" );
+			Log.Info( "Pow" );
 		}
+
 		public override void AttackSecondary()
 		{
-
 			Log.Info( "Pew" );
 		}
-
-		private void processAttack( WizzardPlayer playerEnt )
+		private void ProcessSlotstButtons()
+		{
+			if ( Input.Pressed( InputButton.Slot1 ) )
+				Fire = !Fire;
+			if ( Input.Pressed( InputButton.Slot2 ) )
+				Earth = !Earth;
+			if ( Input.Pressed( InputButton.Slot3 ) )
+				Lightning = !Lightning;
+			if ( Input.Pressed( InputButton.Slot4 ) )
+				Life = !Life;
+		}
+		private void ProcessMana()
+		{
+			if ( Mana > 100 )
+				Mana = 100;
+			if ( Mana < 0 )
+				Mana = 0;
+			if ( Mana < 100 )
+				Mana += .008f;
+		}
+		private void processAttack()
 		{
 			int attackSum = 0;
-			if ( playerEnt.Fire ) attackSum += 1;
-			if ( playerEnt.Earth ) attackSum += 2;
-			if ( playerEnt.Lightning ) attackSum += 4;
-			if ( playerEnt.Life ) attackSum += 8;
+			if ( Fire ) attackSum += 1;
+			if ( Earth ) attackSum += 2;
+			if ( Lightning ) attackSum += 4;
+			if ( Life ) attackSum += 8;
+
 			switch ( attackSum )
 			{
 				case 0:// Base -> wind gust
@@ -97,66 +119,88 @@ namespace Tomast1337
 		{
 
 		}
+
 		private void Flamethrower()
 		{
 
 		}
+
 		private void Rockboulder()
 		{
 
 		}
+
 		private void Meteor()
 		{
 
 		}
+
 		private void Lightningstrike()
 		{
 
 		}
+
 		private void LaserShot()
 		{
 
 		}
+
 		private void StoneSMG()
 		{
 
 		}
+
 		private void StoneShotgun()
 		{
 
 		}
+
 		private void Heal()
 		{
 
 		}
+
 		private void FlameShield()
 		{
 
 		}
+
 		private void RockWall()
 		{
 
 		}
+
 		private void FireWall()
 		{
 
 		}
+
 		private void ForceField()
 		{
 
 		}
+
 		private void VampirismShot()
 		{
 
 		}
+
 		private void Tree()
 		{
 
 		}
+
 		private void Suicide()
 		{
+			Vector3 location = Owner.Position;
+		}
+		public override void Simulate( Client player )
+		{
+			base.Simulate( player );
+			ProcessMana();
+			ProcessSlotstButtons();
+			processAttack();
 
 		}
-
 	}
 }
