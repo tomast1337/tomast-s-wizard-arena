b7ac7c18c490681fe91f488427a49eca63cbba02
Tomast1337 <vycasnicolas@gmail.com>
2021-08-28 03:59:59 -0300

    Formatted code

--- code/Game.cs
+++ code/Game.cs
@@ -1,26 +1,32 @@
-using Sandbox;
-namespace Tomast1337 {
+namespace Tomast1337
+{
+	using Sandbox;
+
 	public partial class Game : Sandbox.Game
 	{
-		public Game(){
+		public Game()
+		{
 			Precache.Add( "weapons/rust_pumpshotgun/rust_pumpshotgun.vmdl" );
 			Precache.Add( "materials/effects/lightcookie.vtex" );
 
-			if (IsServer){
+			if ( IsServer )
+			{
 				Log.Info( "Created in Serverside" );
 				new WizzadHudEntity();
 			}
-			if(IsClient){
-				Log.Info("Created in Clientside");
+			if ( IsClient )
+			{
+				Log.Info( "Created in Clientside" );
 			}
 		}
+
 		public override void ClientJoined( Client client )
 		{
 			base.ClientJoined( client );
-			
+
 			var player = new WizzardPlayer();
 			client.Pawn = player;
-			
+
 			player.Respawn();
 		}
 	}
