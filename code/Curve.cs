46d5d0b563a5aece1df0948f6297f354acd9b7af
Tomast1337 <vycasnicolas@gmail.com>
2021-09-01 22:47:29 -0300

    Algumas refatorações

--- code/Curve.cs
+++ code/Curve.cs
@@ -12,7 +12,7 @@
 
 	class Curve
 	{
-		static string[] models = { "models/tree/log0.vmdl_c",
+		private static string[] models = { "models/tree/log0.vmdl_c",
 		"models/tree/log1.vmdl_c",
 		"models/tree/log2.vmdl_c",
 		"models/tree/log3.vmdl_c"};
@@ -53,7 +53,69 @@
 			return distance >= MaxDistance && distance <= MinDistance;
 		}
 
-		public CurveStates SetNextPoint( Vector3 point, Vector3 PlayerPosition )
+		private Vector3 getTNormal( float t )
+		{
+			float tt = t * t;
+			return (Point0 * (-3 * tt + 6 * t - 3) +
+				Point1 * (9 * tt - 12 * t + 3) +
+				Point2 * (-9 * tt + 6 * t) +
+				Point3 * 3 * tt).Normal;
+		}
+
+		private Vector3 getTPoint( float t )
+		{
+			float ttt = t * t * t;
+			float tt = t * t;
+			return Point0 * (-ttt + 3 * tt - 3 * t + 1) +
+				Point1 * (3 * ttt - 6 * tt + 3 * t) +
+				Point2 * (-3 * ttt + 3 * tt) +
+				Point3 * ttt;
+		}
+
+		private void BuildSegment( float t )
+		{
+			Vector3 position = getTPoint( t );
+			var prop = new ModelEntity();
+
+
+			prop.SetModel( Rand.FromArray<string>( models ) );
+			prop.Position = position;
+			prop.SetupPhysicsFromModel( PhysicsMotionType.Static, false );
+			Vector3 normal = getTNormal( t );
+			prop.Rotation = Rotation.LookAt( normal ).RotateAroundAxis( Vector3.Up, 90 );
+
+			prop.DeleteAsync( 60 );
+			Particles.Create( "particles/smoke_spawn.vpcf", prop.Position );
+			Sound.FromEntity( "tree.spawn", prop );
+		}
+
+		private async void BuildCurve( Vector3 PlayerPosition )
+		{
+			float MaxSize = MaxDistance * 4;
+
+			float TSize = 0.025f;
+			if ( Vector3.DistanceBetween( PlayerPosition, Point0 ) < Vector3.DistanceBetween( PlayerPosition, Point3 ) )
+
+				for ( float t = 0; t < 1; t += TSize )
+				{
+					BuildSegment( t );
+					await Task.Delay( 25 );
+				}
+
+			else
+				for ( float t = 1; t > 0; t -= TSize )
+				{
+					BuildSegment( t );
+					await Task.Delay( 25 );
+				}
+			// Reset
+			Point0 = Vector3.Zero;
+			Point1 = Vector3.Zero;
+			Point2 = Vector3.Zero;
+			Point3 = Vector3.Zero;
+		}
+
+		public CurveStates SetNextPoint( Vector3 point, Entity Owner )
 		{
 			if ( Point0 == Vector3.Zero )
 			{
@@ -86,72 +148,10 @@
 				if ( IsaValidDistace( point, Point2 ) )
 				{
 					Point3 = point;
-					BuildCurve( PlayerPosition );
+					BuildCurve( Owner.Position );
 					return CurveStates.Build;
 				}
 			return CurveStates.InvalidPoint;
-		}
-
-		private void BuildSegment( float t )
-		{
-			Vector3 position = getPoint( t );
-			var prop = new ModelEntity();
-			
-			
-			prop.SetModel( Rand.FromArray<string>(models) );
-			prop.Position = position;
-			prop.SetupPhysicsFromModel( PhysicsMotionType.Static, false );
-			Vector3 normal = getNormal( t );
-			prop.Rotation = Rotation.LookAt( normal ).RotateAroundAxis( Vector3.Up, 90 );
-	
-			prop.DeleteAsync( 45 );
-			Particles.Create( "particles/smoke_spawn.vpcf", prop.Position );
-			Sound.FromEntity( "tree.spawn", prop );
-		}
-
-		private async void BuildCurve( Vector3 PlayerPosition )
-		{
-			float MaxSize = MaxDistance * 4;
-
-			float TSize = 0.025f;
-			if ( Vector3.DistanceBetween( PlayerPosition, Point0 ) < Vector3.DistanceBetween( PlayerPosition, Point3 ) )
-
-				for ( float t = 0; t < 1; t += TSize )
-				{
-					BuildSegment( t );
-					await Task.Delay( 50 );
-				}
-
-			else
-				for ( float t = 1; t > 0; t -= TSize )
-				{
-					BuildSegment( t );
-					await Task.Delay( 50 );
-				}
-			// Resete
-			Point0 = Vector3.Zero;
-			Point1 = Vector3.Zero;
-			Point2 = Vector3.Zero;
-			Point3 = Vector3.Zero;
-		}
-
-		private Vector3 getNormal( float t )
-		{
-			float tt = t * t;
-			return (Point0 * (-3 * tt + 6 * t - 3) +
-				Point1 * (9 * tt - 12 * t + 3) +
-				Point2 * (-9 * tt + 6 * t) +
-				Point3 * 3 * tt).Normal;
-		}
-
-		private Vector3 getPoint( float t )
-		{
-			float ttt = t * t * t;
-			float tt = t * t;
-			return Point0 * (-ttt + 3 * tt - 3 * t + 1) +
-				Point1 * (3 * ttt - 6 * tt + 3 * t) +
-				Point2 * (-3 * ttt + 3 * tt) +
-				Point3 * ttt;
 		}
 	}
 }
